f5178b987c8102e09d90d153e217f7914b0b8b6b
chorm_lmt <chorm@chorm.com>
2018-09-26 23:24:55 +0800

    daily commit..

--- app/src/main/java/com/chorm/ademo/views/Watch.java
+++ app/src/main/java/com/chorm/ademo/views/Watch.java
@@ -4,20 +4,27 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.chorm.ademo.tools.Logger;
 
 public class Watch extends View {
 
     private static final String TAG = "Watch";
 
-    public Watch(Context context) {
-        super(context);
+    private Shape mShape;
 
-    }
+    /**The point of the view's center.*/
+    private Point centerOfPoint;
 
     public Watch(Context context, AttributeSet attrs) {
         super(context, attrs);
+        Logger.debug(TAG, "new Watch(Context,attrs)");
+        mShape = new Shape();
+        centerOfPoint = new Point();
     }
 
     @Override
@@ -26,6 +33,7 @@ public class Watch extends View {
         //set background transparent.
         canvas.drawColor(Color.alpha(0));
         //1. out border.
+        mShape.drawOutBorder(canvas);
 
         //2. bottle layer.
 
@@ -40,4 +48,24 @@ public class Watch extends View {
         //7. glass cover.
 
     }
+
+    private class Shape {
+
+        Shape(){
+            Logger.debug(TAG, "new Shape()");
+        }
+
+        void drawOutBorder(Canvas canvas){
+            Logger.debug(TAG, "draw out border...");
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setColor(Color.YELLOW);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(5);
+
+            canvas.drawCircle(, ,, paint);
+        }
+
+    }
+
 }
